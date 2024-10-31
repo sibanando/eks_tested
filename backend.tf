a1302326722aeca5ce76a3b9c41c72e8fa874734
sibanando <30410459+sibanando@users.noreply.github.com>
2024-10-31 17:48:35 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "testayush"
-    key    = "pharma/terraform.tfstate"
-    region = "us-west-2"
-    encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
-}
