3e1cb5bb330acfe3f35e0c3e1ca5fb796772c337
sibanando <30410459+sibanando@users.noreply.github.com>
2024-10-31 11:30:47 +0530

    Update 7-nodes.tf

--- 7-nodes.tf
+++ 7-nodes.tf
@@ -34,8 +34,8 @@ resource "aws_eks_node_group" "private-nodes" {
   node_role_arn   = aws_iam_role.nodes.arn
 
   subnet_ids = [
-    aws_subnet.pharma_private-us-east-1a.id,
-    aws_subnet.pharma_private-us-east-1b.id
+    aws_subnet.pharma_private-us-west-2a.id,
+    aws_subnet.pharma_private-us-west-2b.id
   ]
 
   capacity_type  = "ON_DEMAND"
