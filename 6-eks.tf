7e2445da90d649656f9f9482920f9cbee3017229
sibanando <30410459+sibanando@users.noreply.github.com>
2024-11-01 20:07:36 +0530

    Update 6-eks.tf

--- 6-eks.tf
+++ 6-eks.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
 }
 
 resource "aws_eks_cluster" "demo" {
-  name     = "demo"
+  name     = "demo-1"
   role_arn = aws_iam_role.demo.arn
 
   vpc_config {
